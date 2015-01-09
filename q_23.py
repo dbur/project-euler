16d0eb31f4b59228f631bf6066f38c29d82baec5
Daniel Burke <danburke@gmail.com>
2015-01-09 02:02:14 -0500

    not much betterr for q_23

--- q_23.py
+++ q_23.py
@@ -1,23 +1,38 @@
+import math
+
 def is_abundant(the_num):
-    the_sum=0
-    for i in range(the_num-1,1,-1):
+    the_sum=1
+    for i in range(math.floor(math.sqrt(the_num)),1,-1):
         if the_num%i==0:
-            the_sum+=i
+            if i == the_num/i:
+                the_sum+=i
+            else:
+                the_sum+=i+(the_num/i)
             if the_sum>the_num:
                 return 1
     return 0
 
 sum_of_abundant_nums = []
 abundant_nums = []
-the_answer = 0
-for i in range(1,28123+1):
+the_answer = 1
+next_sum_to_check=0
+for i in range(2,28125):
     if is_abundant(i):
         abundant_nums.append(i)
-        for z in abundant_nums:
-            sum_of_abundant_nums.append(z+i)
-    if i in sum_of_abundant_nums:
+        for a in range(0,len(abundant_nums)):
+            sum_of_abundant_nums.append(i+abundant_nums[a])
+        sum_of_abundant_nums = sorted(sum_of_abundant_nums)
+    if abundant_nums == []:
+        the_answer += i
+        continue
+    if i == sum_of_abundant_nums[next_sum_to_check]:
+        while i==sum_of_abundant_nums[next_sum_to_check]:
+            next_sum_to_check+=1
         continue
     the_answer += i
     print(i)
 
 print(the_answer)
+print(abundant_nums[0:10])
+
+
